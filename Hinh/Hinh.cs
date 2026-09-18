13adb2a18f96353decec0f7dce5c9f2d5cdf431b
Đinh Việt Kỳ <dky3043@gmail.com>
2026-09-18 13:41:05 +0700

    Update Hinh.cs

--- Hinh/Hinh.cs
+++ Hinh/Hinh.cs
@@ -1,37 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-
-namespace ExampleCAdvance.Hinh;
-
-public abstract class Shape
+namespace ConsoleApp1.Hinh
 {
-    public string Name { get; set; } = string.Empty;
-
-    protected Shape()
+    public interface Hinh
     {
-        Name = "Shape";
+        double GetDienTich();
+
+        double GetChuVi();
+
+        void Nhap();
+
+        void HienThi();
     }
-
-    protected Shape(string name)
-    {
-        Name = name;
-    }
-
-    // Abstract methods to be implemented by specific shapes
-    public abstract double Tinhdientich();
-    public abstract double Tinhchuvi();
-
-    public virtual void DisplayInfo()
-    {
-        Console.WriteLine($"Shape: {Name} | Area: {Tinhdientich():F2} | Perimeter: {Tinhchuvi():F2}");
-    }
-}
-
-// Alias for plural naming if referenced as Shapes
-public abstract class Shapes : Shape
-{
-    protected Shapes() : base() { }
-    protected Shapes(string name) : base(name) { }
 }
